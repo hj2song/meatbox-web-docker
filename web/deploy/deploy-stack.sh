8c681189633e12eea4fd1e36ee55d3d075fb5bd5
meatbox <>
2024-08-01 14:02:28 +0900

    stack 배포 명령어 수정

--- web/deploy/deploy-stack.sh
+++ web/deploy/deploy-stack.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 
-ENV=prod
+ENV="dev"
 IMAGE_REPO="nexus.meatbox.co.kr/${ENV}/meatbox"
-IMAGE_NAME="office"
+IMAGE_NAME="web"
 
 # deploy to swarm cluster
-IMAGE_FULL_NAME="${IMAGE_REPO}/${IMAGE_NAME}:latest" docker stack deploy --with-registry-auth -c ./docker-compose.yml office
+IMAGE_FULL_NAME="${IMAGE_REPO}/${IMAGE_NAME}:latest" docker stack deploy --with-registry-auth -c ./docker-compose.yml ${ENV}-${IMAGE_NAME}
