da7a650fc0a768d56891cfbfdde62b206276cba0
meatbox <>
2024-08-02 10:48:57 +0900

    도커 서비스 명 변경

--- web/deploy/deploy-stack.sh
+++ web/deploy/deploy-stack.sh
@@ -3,7 +3,7 @@
 
 ENV=prod
 IMAGE_REPO="nexus.meatbox.co.kr/${ENV}/meatbox"
-IMAGE_NAME="office"
+IMAGE_NAME="web"
 
 # deploy to swarm cluster
-IMAGE_FULL_NAME="${IMAGE_REPO}/${IMAGE_NAME}:latest" docker stack deploy --with-registry-auth -c ./docker-compose.yml office
+IMAGE_FULL_NAME="${IMAGE_REPO}/${IMAGE_NAME}:latest" docker stack deploy --with-registry-auth -c ./docker-compose.yml ${IMAGE_NAME}
