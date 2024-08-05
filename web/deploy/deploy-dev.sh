c5a12211e24d6ce72308ed858d7df22108cdf58b
meatbox <>
2024-08-05 09:57:43 +0900

    오탈자 수정

--- web/deploy/deploy-dev.sh
+++ web/deploy/deploy-dev.sh
@@ -59,7 +59,7 @@ echo "배포날짜: $(TZ='Asia/Seoul' date +"%Y-%m-%d %H:%M:%S.%3N")" > ${RESOUR
 sed -i 's|\${sys:catalina.home}/logs|\${sys:catalina.home}/logs/\${sys:HOSTNAME}|g' ${LOG4J_PATH}/log4j-web\(DEV\).xml || exit
 
 # Docker Build에 필요한 파일을 DEPLOY 경로로 이동
-cp -rp ${DOCKER_PATH}/${APP_NAME}/deploy/tomcat-dev ${DEPLOY_PATH}/tomcat || exit
+cp -rp ${DOCKER_PATH}/${APP_NAME}/deploy/tomcat-dev ${DEPLOY_PATH}/tomcat-dev || exit
 cp -rp ${DOCKER_PATH}/${APP_NAME}/deploy/build-local-dev.xml ${DEPLOY_PATH}/build.xml || exit
 cp -rp ${DOCKER_PATH}/${APP_NAME}/deploy/Dockerfile-${APP_NAME}-local-${ENV} ${DEPLOY_PATH}/Dockerfile-${APP_NAME}-local || exit
 cp -rp ${DOCKER_PATH}/${APP_NAME}/deploy/.dockerignore ${DEPLOY_PATH}/.dockerignore || exit
