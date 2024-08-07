2f81be46f3eaab494981ed89473a1ccf9613a460
meatbox <>
2024-08-08 08:28:19 +0900

    minify 및 entry.sh 수정

--- web/deploy/tomcat-dev/bin/entry.sh
+++ web/deploy/tomcat-dev/bin/entry.sh
@@ -2,10 +2,12 @@
 
 link.sh
 
-trap `exitHandler` EXIT
+
 exitHandler() {
     echo "Server will be shutdown" 1>&2
     kill -TERM $(ps -ef | grep java | grep -v grep | awk '{print $1}')
 }
+
+trap `exitHandler` EXIT
 
 catalina.sh run
