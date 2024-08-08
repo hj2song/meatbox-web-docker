8201a061e743ab4630fbebef7c2eb023c8132dac
meatbox <>
2024-08-08 12:19:05 +0900

    헬스체크 스크립트 수정

--- web/deploy/tomcat/bin/health_check.sh
+++ web/deploy/tomcat/bin/health_check.sh
@@ -11,15 +11,9 @@ CURL="http://localhost:${PORT}/health"
 
 result=$(curl -s -o /dev/null -w "%{http_code}" --max-time 2 ${CURL})
 if [ "$result" == "200" ]; then
-    http_status="TRUE"
     echo ${CURL}" - web check success"
     exit 0
 else
     echo "fail - "${CURL}" http_code : "${result}
-fi
-if [ "FAIL" == "${http_status}" ]; then
-    echo "fail - "${CURL}" http_code : "${result}
-    echo "End Web Check\n"
     exit 1
 fi
-echo "web service check end"
