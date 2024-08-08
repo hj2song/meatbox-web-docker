7043512dbed88667f63e8e341be37d732e29ac64
meatbox <>
2024-08-08 10:30:15 +0900

    심볼릭링크 설정

--- web/deploy/tomcat-dev/bin/link.sh
+++ web/deploy/tomcat-dev/bin/link.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-# ln -s /home/DATA/share/meatbox/naver/naver_shopping_sales_index_ep.tsv /home/DATA/www-deploy/meatbox-web2/src/main/webapp/naver_shopping_sales_index_ep.tsv
-# ln -s /home/DATA/share/meatbox/naver/naver_ep.tsv /home/DATA/www-deploy/meatbox-web2/src/main/webapp/naver_ep.tsv 
+ln -s /home/DATA/share/meatbox/naver/naver_shopping_sales_index_ep.tsv /home/DATA/www-deploy/meatbox-web2/src/main/webapp/naver_shopping_sales_index_ep.tsv
+ln -s /home/DATA/share/meatbox/naver/naver_ep.tsv /home/DATA/www-deploy/meatbox-web2/src/main/webapp/naver_ep.tsv 
 ln -s /home/DATA/share/meatbox/images /home/DATA/www-deploy/meatbox-web2/src/main/webapp/web/images
 ln -s /home/DATA/share/meatbox/images /home/DATA/www-deploy/meatbox-web2/src/main/webapp/m/images
