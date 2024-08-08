2b621abe0c7658568cc3d6b84499469f4fc2f2c3
meatbox <>
2024-08-08 11:29:22 +0900

    헬스체크 스크립트 변경

--- web/deploy/tomcat-dev/bin/health_check.sh
+++ web/deploy/tomcat-dev/bin/health_check.sh
@@ -9,24 +9,14 @@ fi
 
 CURL="http://localhost:${PORT}/health"
 
-echo "Start Web Check"
-loop=0
-http_status="FAIL"
-echo "web service check start"
-
-while [ ${loop} -le 30 ]; do
-    result=$(curl -s -o /dev/null -w "%{http_code}" --max-time 2 ${CURL})
-    if [ "$result" == "200" ]; then
-        http_status="TRUE"
-        echo ${CURL}" - web check success"
-        break
-    else
-        echo "web check trying ... "${loop}"/30"
-    fi
-    loop=$((loop+1))
-    sleep 1
-done
-
+result=$(curl -s -o /dev/null -w "%{http_code}" --max-time 2 ${CURL})
+if [ "$result" == "200" ]; then
+    http_status="TRUE"
+    echo ${CURL}" - web check success"
+    exit 0
+else
+    echo "fail - "${CURL}" http_code : "${result}
+fi
 if [ "FAIL" == "${http_status}" ]; then
     echo "fail - "${CURL}" http_code : "${result}
     echo "End Web Check\n"
