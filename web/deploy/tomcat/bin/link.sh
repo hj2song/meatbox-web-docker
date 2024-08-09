f83a0ab87d42ec96379f8c13ae6f93b85c5f1719
meatbox <>
2024-08-09 15:42:58 +0900

    오탈자 수정

--- web/deploy/tomcat/bin/link.sh
+++ web/deploy/tomcat/bin/link.sh
@@ -1,8 +1,6 @@
 #!/bin/sh
 
-mkdir /home/DATA/www-deploy/meatbox-web2/src/main/webapp/web /home/DATA/www-deploy/meatbox-web2/src/main/webapp/m
-chown www:www -R /home/DATA/www-deploy/meatbox-web2
-ln -s /home/DATA/share/meatbox/naver/naver_shopping_sales_index_ep.tsv /home/DATA/www-deploy/meatbox-m2/src/main/webapp/naver_shopping_sales_index_ep.tsv
-ln -s /home/DATA/share/meatbox/naver/naver_ep.tsv /home/DATA/www-deploy/meatbox-m2/src/main/webapp/naver_ep.tsv 
-ln -s /home/DATA/share/meatbox/images /home/DATA/www-deploy/meatbox-m2/src/main/webapp/web/images
-ln -s /home/DATA/share/meatbox/images /home/DATA/www-deploy/meatbox-m2/src/main/webapp/m/images
+ln -s /home/DATA/share/meatbox/naver/naver_shopping_sales_index_ep.tsv /home/DATA/www-deploy/meatbox-web2/src/main/webapp/naver_shopping_sales_index_ep.tsv
+ln -s /home/DATA/share/meatbox/naver/naver_ep.tsv /home/DATA/www-deploy/meatbox-web2/src/main/webapp/naver_ep.tsv 
+ln -s /home/DATA/share/meatbox/images /home/DATA/www-deploy/meatbox-web2/src/main/webapp/web/images
+ln -s /home/DATA/share/meatbox/images /home/DATA/www-deploy/meatbox-web2/src/main/webapp/m/images
