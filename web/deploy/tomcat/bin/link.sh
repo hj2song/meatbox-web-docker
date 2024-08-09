3b6ef0ddf52ff6c42f8d930de8c6e97d3ac5d4f3
meatbox <>
2024-08-09 15:51:29 +0900

    link.sh에 디렉토리 생성 추가

--- web/deploy/tomcat/bin/link.sh
+++ web/deploy/tomcat/bin/link.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+mkdir /home/DATA/www-deploy/meatbox-web2/src/main/webapp/web /home/DATA/www-deploy/meatbox-web2/src/main/webapp/m
 ln -s /home/DATA/share/meatbox/naver/naver_shopping_sales_index_ep.tsv /home/DATA/www-deploy/meatbox-web2/src/main/webapp/naver_shopping_sales_index_ep.tsv
 ln -s /home/DATA/share/meatbox/naver/naver_ep.tsv /home/DATA/www-deploy/meatbox-web2/src/main/webapp/naver_ep.tsv 
 ln -s /home/DATA/share/meatbox/images /home/DATA/www-deploy/meatbox-web2/src/main/webapp/web/images
